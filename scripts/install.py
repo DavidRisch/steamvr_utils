b5cb9d4553fc170590e0c9b4589746f5d9c9ddff
David Risch <acc_mid@rischnet.de>
2021-01-09 17:21:38 +0100

    Fix ugly paths generated by install

--- scripts/install.py
+++ scripts/install.py
@@ -4,9 +4,7 @@ import os
 import pathlib
 
 
-def print_launch_options():
-    steamvr_utils_path = os.path.join(os.path.dirname(__file__), 'steamvr_utils.py')
-
+def print_launch_options(steamvr_utils_path):
     launch_options = 'python3 {steamvr_utils_path} daemon  >>/tmp/steamvr_utils.out 2>&1; %command%;'.format(
         steamvr_utils_path=steamvr_utils_path)
 
@@ -29,9 +27,10 @@ sudo pip3 install bluepy
     import steamvr_utils
 
 
-def create_desktop_file(action):
-    command = 'python3 {} {}'.format(os.path.join(os.path.dirname(__file__), 'steamvr_utils.py'), action)
+def create_desktop_file(action, steamvr_utils_path):
+    command = 'python3 {} {}'.format(steamvr_utils_path, action)
     image_path = os.path.join(os.path.dirname(__file__), '..', 'images', 'icon_{}.png'.format(action))
+    image_path = os.path.realpath(image_path)
 
     content = '''[Desktop Entry]
 Name=steamvr_utils {action}
@@ -44,19 +43,22 @@ Icon={image_path}
 '''.format(action=action, command=command, image_path=image_path)
 
     desktop_file_path = os.path.join(pathlib.Path.home(), '.local/share/applications/',
-                             'steamvr_utils_{}.desktop'.format(action))
+                                     'steamvr_utils_{}.desktop'.format(action))
 
     with open(desktop_file_path, "w") as desktop_file:
         desktop_file.write(content)
 
 
 def main():
-    print_launch_options()
+    steamvr_utils_path = os.path.join(os.path.dirname(__file__), 'steamvr_utils.py')
+    steamvr_utils_path = os.path.realpath(steamvr_utils_path)
+
+    print_launch_options(steamvr_utils_path)
 
     test_imports()
 
-    create_desktop_file('on')
-    create_desktop_file('off')
+    create_desktop_file('on', steamvr_utils_path)
+    create_desktop_file('off', steamvr_utils_path)
 
 
 if __name__ == '__main__':
